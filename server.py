c49336617f7d35aa2614b0cca1b0d7cd96705d63
Michele Bontempi <admin@mbontempi.dev>
2024-11-24 18:12:50 -0500

    code improvements for pylint in server.py

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+''' 
+Flask-based Emotion Detection API that analyzes a given text 
+to identify the emotions expressed within it.
+'''
 from flask import Flask, request
 
 from EmotionDetection import emotion_detection as ed
@@ -6,12 +10,18 @@ app = Flask('Emotion Detection App')
 
 @app.route("/emotionDetector")
 def emotion_detector():
+    ''' 
+    GET /emotionDetector
+    request args:
+        text -> a string containing the input statement to be analized
+    '''
+
     text = request.args.get('text')
     res = ed.emotion_detector(text)
 
     if res['dominant_emotion'] is None:
         return 'Invalid text! Please try again!'
-        
+
     return ('For the given statement, the system response is '
         f"'anger': {res['anger']}, 'disgust': {res['disgust']}, "
         f"'fear\': {res['fear']}, \'joy\': {res['joy']} and "
